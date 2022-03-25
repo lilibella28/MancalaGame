a58e3fea6ade17ddf0792ed581335c2ae91cae2a
lilibella <lilibella28@hotmail.com>
2022-03-24 23:17:59 -0700

    added layout

--- JS/app.js
+++ JS/app.js
@@ -1,46 +1,21 @@
-// These file will hold all of the user interactions
 
-// =========These file will only hold the functions and functions helper for the game
 
-/* 
-================================================================================================
-
-Constanss Variable
-================================================================================================
-*/
 
 const mainArrayNumber = [4, 4, 4, 4, 4, 4, 0, 4, 4, 4, 4, 4, 4, 0];
-
 const sumMainArray = mainArrayNumber.reduce((acc, num) => {
   acc = acc + num;
   return acc;
 }, 0);
 
-
-//The sumMainArray Count all the element inside the array and return 48,
-//These value is important to create the recursion call for ending the game.
-
-/* 
-================================================================================================
-
-States Variable
-================================================================================================
-*/
-//The value of these score will keep updating based on the value inside the arraysAtindex[7] and [14]
-let player1Score = 0;
-let player2Score = 0;
+let player1Score;
+let player2Score;
 let indexUser;
 let currentPlayer;
 
-// let remaindsScore = sumMainArray; //Setting the remaind score to sum of the element inside tthe array UNSURE IF THESE THE Aproach i will take
-// console.log(sumMainArray)
-/* 
-================================================================================================
-Adding eventlister, (these will add at the end for now)
-================================================================================================
-*/
 
-const buttoms = document.querySelector("section.firstSection  div");
+//Event Listener
+
+const buttoms = document.querySelector("section.firstSection div" );
 let player1ScoreBox = document.querySelector("#player1S");
 let player2ScoreBox = document.querySelector("#player2S");
 let remaindMarbles = document.querySelector("#remaindsMable");
@@ -51,56 +26,49 @@ let displayMessage = document.querySelector("#message");
 
 buttoms.addEventListener("click", handlerEvent);
 startPlaying.addEventListener("click", init);
-init();
 render()
-/* 
-================================================================================================
-                  Init function 
 
-================================================================================================
-*/
 
+//init
 function init(e) {
-  displayMessage.innerHTML = "Player one turn";
   remaindMarbles.innerHTML = sumMainArray;
-  player1ScoreBox.innerHTML = 0;
-  player2ScoreBox.innerHTML = 0;
-
-  render()
+  player1ScoreBox = mainArrayNumber[6];
+  player2ScoreBox = mainArrayNumber[13];
+  render();
+  currentPlayer = true;
+  turns()
 }
 
-/* 
-
-================================================================================================
-
-          RENDER FUNCTION
-================================================================================================
-*/
+//Render Function
 
 function render() {
+  
   mainArrayNumber.forEach((num) => {
     const newBtn = document.querySelector("button");
     buttoms.appendChild(newBtn);
-    // console.log(newBtn);
     return (newBtn.innerHTML = num);
   });
   
-  winner()
 }
 
-function handlerEvent(e) {
 
+
+//Event Handler
+function handlerEvent(e) {
   indexUser = parseInt(e.target.id);
   const arrayPositionTofill = mainArrayNumber[indexUser];
   mainArrayNumber[indexUser] = 0;
   mancalaAdd(mainArrayNumber, indexUser - 1, arrayPositionTofill);
+  render();
   empthyStorages();
-  scores();
-  render()
-  // displayMessage.innerHTML = "Player Two turn";
+ 
+  
 }
 
-//Thesee function add the initial value of the array to the buttoms.
+//Main function, 
+
+// recursive function to move the value; the user choose,
+
 
 function mancalaAdd(arr, starPoint, position) {
   if (position === 0) {
@@ -110,29 +78,26 @@ function mancalaAdd(arr, starPoint, position) {
     starPoint = arr.length - 1;
   }
   arr[starPoint] += 1;
-
   return mancalaAdd(arr, starPoint - 1, position - 1);
-  
 }
 
 function scores() {
   if (mainArrayNumber[6] > 0) {
-    player1Score = mainArrayNumber[6]
-    remaindMarbles = sumMainArray - player2Score 
+    player1Score = mainArrayNumber[6];
+    remaindMarbles = sumMainArray - player2Score;
     return player1Score;
   }
-
   if (mainArrayNumber[13] > 0) {
-     player2Score = mainArrayNumber[13];
-     remaindMarbles = sumMainArray - player2Score 
-     return player2Score
+    player2Score = mainArrayNumber[13];
+    remaindMarbles = sumMainArray - player2Score;
+    return player2Score;
   }
- 
 }
+// Rules goes! Here
 
+//mancala rules 
 function empthyStorages() {
-  //Tried to see if you can create a range function becuase these think look so ugl
- 
+  //Tried to see if you can create a range function becuase these think look so ugly
   if (
     mainArrayNumber[0] === 0 &&
     mainArrayNumber[1] === 0 &&
@@ -147,13 +112,12 @@ function empthyStorages() {
       mainArrayNumber[10] +
       mainArrayNumber[11] +
       mainArrayNumber[12] +
-      mainArrayNumber[13] ;
-    console.log(player2Score);
-    player2ScoreBox.innerHTML = player2Score;
+      mainArrayNumber[13];
+    winner();
     return player2Score;
   }
-  
-  else if (
+
+  if (
     mainArrayNumber[7] === 0 &&
     mainArrayNumber[8] === 0 &&
     mainArrayNumber[9] === 0 &&
@@ -166,59 +130,59 @@ function empthyStorages() {
       mainArrayNumber[2] +
       mainArrayNumber[3] +
       mainArrayNumber[4] +
-      mainArrayNumber[5] + 
-      mainArrayNumber[6]  ;
-    console.log(player1Score);
-    player1ScoreBox.innerHTML = player1Score;
+      mainArrayNumber[5] +
+      mainArrayNumber[6];
+    winner();
     return player1Score;
   }
-
-   singleOne()
+  scores()
+  render();
+ 
 }
-
+//Rules check if the user at the last index 
+function singleOne(){
+  if (mainArrayNumber[5] === 1) {
+    mainArrayNumber[6] = mainArrayNumber[7];
+    mainArrayNumber[7] = 0;
+    player1Score = mainArrayNumber[6];
+    return player1Score;
+  }  if (mainArrayNumber[12] === 1) {
+    mainArrayNumber[13] = mainArrayNumber[0];
+    mainArrayNumber[0] = 0;
+    player2Score = mainArrayNumber[13];
+    return player2Score;
+  }
+}
 
 function winner() {
   if (player1Score > player2Score) {
-    console.log(player2Score)
-    displayMessage.innerHTML = `player 1 is  winning with a score of ${player1Score}`;
+    displayMessage.innerHTML = `player 1 is  the winner with a score of ${player1Score}`;
     return displayMessage;
   }
   if (player2Score > player1Score) {
-    displayMessage.innerHTML = `player 2 is winning with a score of ${player2Score}`;
+    displayMessage.innerHTML = `player 2 is the winner with a score of ${player2Score}`;
     return displayMessage;
+  } else {
+    return (displayMessage.innerHTML = "Tied Game");
   }
-   else {
-    return displayMessage.innerHTML = "Tied Game";
- 
+}
+
+
+
+
+function turns(){
+
+  if(currentPlayer === true){
+
+    return displayMessage.innerHTML = "Player One turn"
   }
-  
+
+  if(currentPlayer === false){
+    return displayMessage.innerHTML = "Player Two turn"
+  }
 }
 
 
 
-function singleOne(){
 
-  if(mainArrayNumber[5] === 1){
-    mainArrayNumber[6] = mainArrayNumber[12]
-    mainArrayNumber[12] = 0
-    player1Score = mainArrayNumber[6]
-    return player1Score
-}
-else if(mainArrayNumber[12] === 1){
-  mainArrayNumber[13] = mainArrayNumber[5]
-  mainArrayNumber[5] = 0
-  player2Score = mainArrayNumber[13]
-  return player2Score
-}
-
-}
-
-
-
-function playersTurns(){
-    if(currentPlayer === 1){
-      return 
-    }
-
-}
 
